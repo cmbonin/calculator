4749d4f71c68a478123986ce41aaab4ab3fead7f
celine <bonin.c.m@gmail.com>
2018-04-04 11:00:49 +1000

    Cleaned up styling and grid

--- src/Calculator/component.jsx
+++ src/Calculator/component.jsx
@@ -3,30 +3,29 @@ import styles from './styles.scss';
 
 export default class Calulator extends React.Component {
   render() {
-    console.log(styles)
     return (
-      <div className="content">
+      <div className='content'>
         <div className={styles.grid}>
-          <div id="display" className="display"></div>
-          <button id="btn-equals" className={styles.btnEquals}>=</button>
-          <button id="btn-plus" className="btn-plus">+</button>
-          <button id="btn-minus" className="btn-minus">-</button>
-          <button id="btn-times" className="btn-times">x</button>
-          <button id="btn-divide" className="btn-divide"></button>
-          <button id="btn-clear" className="btn-clear">C</button>
-          <button id="btn-del" className="btn-del">DEL</button>
-          <button id="btn-zero" className="btn-zero">0</button>
-          <button id="btn-one" className="btn-one">1</button>
-          <button id="btn-two" className="btn-two">2</button>
-          <button id="btn-three" className="btn-three">3</button>
-          <button id="btn-four" className="btn-four">4</button>
-          <button id="btn-five" className="btn-five">5</button>
-          <button id="btn-six" className="btn-six">6</button>
-          <button id="btn-seven" className="btn-seven">7</button>
-          <button id="btn-eight" className="btn-eight">8</button>
-          <button id="btn-nine" className="btn-nine">9</button>
-          <button id="btn-dot" className="btn-dot">.</button>
-          <button id="btn-pos-neg" className="btn-pos-neg">+/-</button>
+          <div id='display' className={styles.display}></div>
+          <button id='btn-equals' className={`${styles.btnEquals} btn`}>=</button>
+          <button id='btn-clear' className='btn'>C</button>
+          <button id='btn-del' className='btn'>DEL</button>
+          <button id='btn-pos-neg' className='btn'>+/-</button>
+          <button id='btn-plus' className='btn'>+</button>
+          <button id='btn-one' className='btn'>1</button>
+          <button id='btn-two' className='btn'>2</button>
+          <button id='btn-three' className='btn'>3</button>
+          <button id='btn-minus' className='btn'>-</button>
+          <button id='btn-four' className='btn'>4</button>
+          <button id='btn-five' className='btn'>5</button>
+          <button id='btn-six' className='btn'>6</button>
+          <button id='btn-times' className='btn'>x</button>
+          <button id='btn-seven' className='btn'>7</button>
+          <button id='btn-eight' className='btn'>8</button>
+          <button id='btn-nine' className='btn'>9</button>
+          <button id='btn-divide' className='btn'>/</button>
+          <button id='btn-zero' className='btn'>0</button>
+          <button id='btn-dot' className='btn'>.</button>
         </div>
       </div>
     );
